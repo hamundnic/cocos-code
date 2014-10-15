be3e9c7c91cbd21e1028833d05b56233b789c994
hanson <hulefei29@gmail.com>
2014-10-15 11:20:01 +0800

    modify

--- gdgame/frameworks/cocos2d-x/cocos/scripting/lua-bindings/auto/api/lua_cocos2dx_custom_auto_api.lua
+++ gdgame/frameworks/cocos2d-x/cocos/scripting/lua-bindings/auto/api/lua_cocos2dx_custom_auto_api.lua
@@ -1,4 +1,6 @@
 
 
 
+
+
 return nil
--- gdgame/frameworks/cocos2d-x/cocos/scripting/lua-bindings/auto/lua_cocos2dx_custom_auto.cpp
+++ gdgame/frameworks/cocos2d-x/cocos/scripting/lua-bindings/auto/lua_cocos2dx_custom_auto.cpp
@@ -1,5 +1,6 @@
 #include "lua_cocos2dx_custom_auto.hpp"
 #include "CustomClass.h"
+#include "MaskSprite.h"
 #include "tolua_fix.h"
 #include "LuaBasicConversions.h"
 
@@ -180,6 +181,155 @@ int lua_register_cocos2dx_custom_CustomClass(lua_State* tolua_S)
     g_typeCast["CustomClass"] = "cc.CustomClass";
     return 1;
 }
+
+int lua_cocos2dx_custom_MaskSprite_setMask(lua_State* tolua_S)
+{
+    int argc = 0;
+    MaskSprite* cobj = nullptr;
+    bool ok  = true;
+
+#if COCOS2D_DEBUG >= 1
+    tolua_Error tolua_err;
+#endif
+
+
+#if COCOS2D_DEBUG >= 1
+    if (!tolua_isusertype(tolua_S,1,"MaskSprite",0,&tolua_err)) goto tolua_lerror;
+#endif
+
+    cobj = (MaskSprite*)tolua_tousertype(tolua_S,1,0);
+
+#if COCOS2D_DEBUG >= 1
+    if (!cobj) 
+    {
+        tolua_error(tolua_S,"invalid 'cobj' in function 'lua_cocos2dx_custom_MaskSprite_setMask'", nullptr);
+        return 0;
+    }
+#endif
+
+    argc = lua_gettop(tolua_S)-1;
+    if (argc == 3) 
+    {
+        std::string arg0;
+        std::string arg1;
+        std::string arg2;
+
+        ok &= luaval_to_std_string(tolua_S, 2,&arg0);
+
+        ok &= luaval_to_std_string(tolua_S, 3,&arg1);
+
+        ok &= luaval_to_std_string(tolua_S, 4,&arg2);
+        if(!ok)
+            return 0;
+        cobj->setMask(arg0, arg1, arg2);
+        return 0;
+    }
+    CCLOG("%s has wrong number of arguments: %d, was expecting %d \n", "setMask",argc, 3);
+    return 0;
+
+#if COCOS2D_DEBUG >= 1
+    tolua_lerror:
+    tolua_error(tolua_S,"#ferror in function 'lua_cocos2dx_custom_MaskSprite_setMask'.",&tolua_err);
+#endif
+
+    return 0;
+}
+int lua_cocos2dx_custom_MaskSprite_create(lua_State* tolua_S)
+{
+    int argc = 0;
+    bool ok  = true;
+#if COCOS2D_DEBUG >= 1
+    tolua_Error tolua_err;
+#endif
+
+#if COCOS2D_DEBUG >= 1
+    if (!tolua_isusertable(tolua_S,1,"MaskSprite",0,&tolua_err)) goto tolua_lerror;
+#endif
+
+    argc = lua_gettop(tolua_S)-1;
+
+    do 
+    {
+        if (argc == 1)
+        {
+            const char* arg0;
+            std::string arg0_tmp; ok &= luaval_to_std_string(tolua_S, 2, &arg0_tmp); arg0 = arg0_tmp.c_str();
+            if (!ok) { break; }
+            MaskSprite* ret = MaskSprite::create(arg0);
+            object_to_luaval<MaskSprite>(tolua_S, "MaskSprite",(MaskSprite*)ret);
+            return 1;
+        }
+    } while (0);
+    ok  = true;
+    do 
+    {
+        if (argc == 0)
+        {
+            MaskSprite* ret = MaskSprite::create();
+            object_to_luaval<MaskSprite>(tolua_S, "MaskSprite",(MaskSprite*)ret);
+            return 1;
+        }
+    } while (0);
+    ok  = true;
+    CCLOG("%s has wrong number of arguments: %d, was expecting %d", "create",argc, 0);
+    return 0;
+#if COCOS2D_DEBUG >= 1
+    tolua_lerror:
+    tolua_error(tolua_S,"#ferror in function 'lua_cocos2dx_custom_MaskSprite_create'.",&tolua_err);
+#endif
+    return 0;
+}
+int lua_cocos2dx_custom_MaskSprite_testBindingIsOK(lua_State* tolua_S)
+{
+    int argc = 0;
+    bool ok  = true;
+
+#if COCOS2D_DEBUG >= 1
+    tolua_Error tolua_err;
+#endif
+
+#if COCOS2D_DEBUG >= 1
+    if (!tolua_isusertable(tolua_S,1,"MaskSprite",0,&tolua_err)) goto tolua_lerror;
+#endif
+
+    argc = lua_gettop(tolua_S) - 1;
+
+    if (argc == 0)
+    {
+        if(!ok)
+            return 0;
+        MaskSprite::testBindingIsOK();
+        return 0;
+    }
+    CCLOG("%s has wrong number of arguments: %d, was expecting %d\n ", "testBindingIsOK",argc, 0);
+    return 0;
+#if COCOS2D_DEBUG >= 1
+    tolua_lerror:
+    tolua_error(tolua_S,"#ferror in function 'lua_cocos2dx_custom_MaskSprite_testBindingIsOK'.",&tolua_err);
+#endif
+    return 0;
+}
+static int lua_cocos2dx_custom_MaskSprite_finalize(lua_State* tolua_S)
+{
+    printf("luabindings: finalizing LUA object (MaskSprite)");
+    return 0;
+}
+
+int lua_register_cocos2dx_custom_MaskSprite(lua_State* tolua_S)
+{
+    tolua_usertype(tolua_S,"MaskSprite");
+    tolua_cclass(tolua_S,"MaskSprite","MaskSprite","cc.Sprite",nullptr);
+
+    tolua_beginmodule(tolua_S,"MaskSprite");
+        tolua_function(tolua_S,"setMask",lua_cocos2dx_custom_MaskSprite_setMask);
+        tolua_function(tolua_S,"create", lua_cocos2dx_custom_MaskSprite_create);
+        tolua_function(tolua_S,"testBindingIsOK", lua_cocos2dx_custom_MaskSprite_testBindingIsOK);
+    tolua_endmodule(tolua_S);
+    std::string typeName = typeid(MaskSprite).name();
+    g_luaType[typeName] = "MaskSprite";
+    g_typeCast["MaskSprite"] = "MaskSprite";
+    return 1;
+}
 TOLUA_API int register_all_cocos2dx_custom(lua_State* tolua_S)
 {
 	tolua_open(tolua_S);
@@ -188,6 +338,7 @@ TOLUA_API int register_all_cocos2dx_custom(lua_State* tolua_S)
 	tolua_beginmodule(tolua_S,nullptr);
 
 	lua_register_cocos2dx_custom_CustomClass(tolua_S);
+	lua_register_cocos2dx_custom_MaskSprite(tolua_S);
 
 	tolua_endmodule(tolua_S);
 	return 1;
